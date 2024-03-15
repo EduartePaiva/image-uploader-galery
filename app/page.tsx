73b61ffc2ba9e9ced29e8e59c71933f967d9cc34
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-03-15 07:11:38 -0300

    Auth added to the project.
    I used an auth provider called clerk.
    Clerk is well implemented with nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import UploadImage from "@/components/UploadImage";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="">
+      <UserButton
+        afterSignOutUrl="/sign-in"
+        afterSwitchSessionUrl="/"
+      />
       <UploadImage />
     </main>
   );
