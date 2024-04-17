d8fd30a188fb330396b0332ad9f3551f46ddec89
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-04-17 17:02:56 -0300

    feat: added the button back to top with nice animation
    and handling the event scroll performance matter.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Main from "./Main"
+import BackToTopButton from "@/components/BackToTopButton"
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <Header />
             <Main />
             <Footer />
+            <BackToTopButton />
         </div>
     )
 }
