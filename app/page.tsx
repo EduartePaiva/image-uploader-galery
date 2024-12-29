f6848e1c09d1aeb8fea21bf3b7d544e7f359bb30
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-12-29 10:16:23 -0300

    renamed files. For some reason in windows I can't do this simple rename

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Header from "./header";
-import Footer from "./footer";
-import Main from "./main";
 import BackToTopButton from "@/components/back-to-top-button";
+
+import Footer from "./footer";
+import Header from "./header";
+import Main from "./main";
 
 export default function Home() {
     return (
-        <div className="min-h-screen grid grid-rows-[min-content_1fr_80px]">
+        <div className="grid min-h-screen grid-rows-[min-content_1fr_80px]">
             <Header />
             <Main />
             <Footer />
